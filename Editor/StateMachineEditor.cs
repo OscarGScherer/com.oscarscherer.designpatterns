155675d5c2a0c76a482eb569af8676623fb97783
OscarGScherer <oscarscherer2020@gmail.com>
2025-10-10 18:24:10 -0300

    Updated StateMachine

--- Editor/StateMachineEditor.cs
+++ Editor/StateMachineEditor.cs
@@ -2,45 +2,89 @@
 using UnityEngine;
 using UnityEditor;
 using System;
+using System.Collections.Generic;
 
 namespace DesignPatterns.StateMachine
 {
     [CustomEditor(typeof(StateMachine), true)]
     public class StateMachineEditor : Editor
     {
-        private static bool showSelectedNodeFields = false, showSelectedNodeProperties = false;
+        private static bool showStateChangeHistory = false, showSelectedNodeFields = false, showSelectedNodeProperties = false;
+        private Vector2 stateChangeHistoryScrollPos;
 
-        public override void OnInspectorGUI() 
+        StateMachine targetSM;
+
+        private void OnEnable()
+        {
+            targetSM = (StateMachine)target;
+            EditorApplication.update += CheckForRepaint;
+        }
+
+        private void OnDisable()
+        {
+            EditorApplication.update -= CheckForRepaint;
+        }
+
+        private void CheckForRepaint()
+        {
+            if (targetSM == null) return;
+            if (!targetSM.refreshInspector) return;
+            Repaint();
+            targetSM.refreshInspector = false;
+        }
+
+        public override void OnInspectorGUI()
         {
             DrawDefaultInspector();
 
-            CustomEditorUtilities.DrawUILine(Color.gray, 0, 10);
-            GUIStyle boldStyle = new GUIStyle(GUI.skin.label);
-            boldStyle.fontStyle = FontStyle.Bold;
-            boldStyle.alignment = TextAnchor.MiddleCenter;
-            EditorGUILayout.LabelField("State machine viewer", boldStyle);
-            CustomEditorUtilities.DrawUILine(Color.gray, 1, 0);
+            CustomEditorUtilities.DrawUILine(Color.gray, 0, 5);
 
-            StateMachine sm = (StateMachine) target;
+            GUIStyle titleStyle = new GUIStyle(GUI.skin.label);
+            titleStyle.fontStyle = FontStyle.Bold;
+            titleStyle.fontSize = 12;
+            EditorGUILayout.LabelField("State machine viewer", titleStyle);
+            CustomEditorUtilities.DrawUILine(Color.gray, 2, 0);
 
-            if(sm.currentStateObject == null)
+            if (targetSM.CurrentStateObject == null)
             {
                 GUILayout.Label("Current state is null");
                 return;
             }
-            
-            GUIStyle stateNameStyle = new GUIStyle(GUI.skin.label){ fontStyle = FontStyle.Bold, alignment = TextAnchor.MiddleLeft };
 
-            GUIStyle textArea = new GUIStyle(GUI.skin.textArea){ wordWrap = true, alignment = TextAnchor.UpperLeft, padding = new RectOffset(20,0,5,10) };
-            textArea.normal = new GUIStyleState(){ textColor = Color.white };
+            GUIStyle boldStyle = new GUIStyle(GUI.skin.label);
+            boldStyle.fontStyle = FontStyle.Bold;
 
-            Type stateType = sm.currentStateObject.GetType();
-            EditorGUILayout.LabelField("Current state: " + stateType.Name, stateNameStyle);
+            GUIStyle stateNameStyle = new GUIStyle(GUI.skin.label) { fontStyle = FontStyle.Bold, alignment = TextAnchor.MiddleLeft };
+            Type stateType = targetSM.CurrentStateObject.GetType();
+            EditorGUILayout.LabelField("Current state -> " + stateType.Name, stateNameStyle);
 
-            showSelectedNodeFields = CustomEditorUtilities.DisplayTargetFieldsWithReflection(sm.currentStateObject, showSelectedNodeFields);
-            // showSelectedNodeProperties = CustomEditorUtilities.DisplayTargetPropertiesWithReflection(sm.currentStateObject, showSelectedNodeProperties);
+            GUIStyle textArea = new GUIStyle(GUI.skin.textArea) { wordWrap = true, alignment = TextAnchor.UpperLeft, padding = new RectOffset(10, 0, 5, 10) };
+            textArea.normal = new GUIStyleState() { textColor = Color.white };
+            textArea.richText = true;
 
-            EditorGUILayout.LabelField(sm.currentStateObject.ToDebugString(), textArea);
+            EditorGUILayout.LabelField(targetSM.CurrentStateObject.ToDebugString(), textArea);
+
+            showStateChangeHistory = EditorGUILayout.Foldout(showStateChangeHistory, "Show history");
+            if (showStateChangeHistory)
+            {
+                GUIStyle boxStyle = new GUIStyle(GUI.skin.box);
+                boxStyle.padding = new RectOffset(10, 10, 10, 10);
+                boxStyle.margin = new RectOffset(0, 0, 10, 10);
+
+                stateChangeHistoryScrollPos = EditorGUILayout.BeginScrollView(stateChangeHistoryScrollPos, textArea, GUILayout.Height(100));
+
+                LinkedListNode<State> state = targetSM.stateChangeHistory.First;
+                for (int i = 0; i < targetSM.stateChangeHistory.Count; i++)
+                {
+                    if (i == 0) EditorGUILayout.LabelField($"> {state.Value.GetType().Name} (current)");
+                    else EditorGUILayout.LabelField($"> {state.Value.GetType().Name}");
+                    state = state.Next;
+                }
+                EditorGUILayout.EndScrollView();
+            }
+
+            showSelectedNodeFields = CustomEditorUtilities.DisplayTargetFieldsWithReflection(targetSM.CurrentStateObject, showSelectedNodeFields);
+            // showSelectedNodeProperties = CustomEditorUtilities.DisplayTargetPropertiesWithReflection(targetSM.CurrentStateObject, showSelectedNodeProperties);
         }
     }
 }
--- Runtime/StateMachine.cs
+++ Runtime/StateMachine.cs
@@ -1,92 +1,167 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-namespace DesignPatterns
+namespace DesignPatterns.StateMachine
 {
-    namespace StateMachine
+    public abstract class State
     {
-        public abstract class State
+        /// <returns> Text to be printed on the inpector, whenver the SM is in this state. </returns>
+        public virtual string ToDebugString() => $"Override <color=\"yellow\">ToDebugString</color> in <color=\"#2dc46cff\">{GetType().Name}</color> to show custom debug input here!";
+        public abstract void InactiveUpdate();
+        public abstract void GeneralUpdate();
+    }
+
+    public class Transition<T>
+    {
+        private Transition(Type type) => this.type = type;
+        public static Transition<T> To<DT  >() where DT : T => new Transition<T>(typeof(DT));
+        public Type type = typeof(T);
+    }
+
+    /// <summary>
+    /// Base state class. Inherit from it to create your own state.
+    /// Each state machine can only have one state of each type of State.
+    /// </summary>
+    public abstract class State<TSM, TS, TSI> : State
+        where TS : State<TSM, TS, TSI>
+        where TSM : StateMachine<TSM, TS, TSI>
+    {
+        /// <summary>
+        /// Called when the SM enters this state.
+        /// </summary>
+        public abstract void OnEnter(TSM stateMachine);
+        /// <summary>
+        /// Called whenever the SM exits this state
+        /// </summary>
+        public abstract void OnExit(TSM stateMachine);
+        /// <summary>
+        /// Implement this to get all necessary references for your state to run.
+        /// This function will be called when a state is added to a state machine.
+        /// </summary>
+        /// <param name="stateMachine"> The state machine the outer SM is a part of </param>
+        public abstract void Initialize(TSM stateMachine);
+        /// <summary>
+        /// Called every frame while the SM is in this state.
+        /// Controls transitions to other states.
+        /// </summary>
+        /// <returns>
+        /// The type of state the SM should transition to.
+        /// </returns>
+        public abstract Transition<TS> ActiveUpdate(TSI context);
+        public override void GeneralUpdate() { /*NOOP*/ }
+        public override void InactiveUpdate() { /*NOOP*/ }
+    }
+
+    [Serializable]
+    public abstract class StateMachine : MonoBehaviour
+    {
+        [NonSerialized] public bool refreshInspector = false;
+        private const int MAX_HISTORY_LENGTH = 100;
+        public LinkedList<State> stateChangeHistory = new LinkedList<State>();
+
+        protected void LogStateChange(State state)
         {
-            /// <returns> Text to be printed on the inpector, whenver the SM is in this state. </returns>
-            public virtual string ToDebugString() => "You can override <color=#FFDE59>ToDebugString</color>() to display custom debug information about your state here!";
+            stateChangeHistory.AddFirst(state);
+            if (stateChangeHistory.Count > MAX_HISTORY_LENGTH) stateChangeHistory.RemoveLast();
+            refreshInspector = true;
         }
 
         /// <summary>
-        /// Base state class. Inherit from it to create your own state.
-        /// Each state machine can only have one state of each type of State.
+        /// Current State object cast to the base State class.
         /// </summary>
-        public abstract class State<TStateMachine, TState, TStateInput> : State
-            where TState : State<TStateMachine, TState, TStateInput>, new()
-            where TStateMachine : StateMachine<TStateMachine, TState, TStateInput>
-        {
-            public abstract void Initialize(TStateMachine stateMachine);
-            /// <summary>
-            /// Called when the SM enters this state.
-            /// </summary>
-            public virtual void OnEnter() { }
-            public abstract TState Update(TStateInput input);
-            /// <summary>
-            /// Called whenever the SM exits this state
-            /// </summary>
-            public virtual void OnExit() { }
-        }
+        public abstract State CurrentStateObject { get; }
+    }
 
-        public abstract class StateMachine : MonoBehaviour
+    /// <summary>
+    /// Base state machine class. Inherit from it to create your own.
+    /// </summary>
+    public abstract class StateMachine<TSM, TS, TSI> : StateMachine
+        where TS : State<TSM, TS, TSI>
+        where TSM : StateMachine<TSM, TS, TSI>
+    {
+        public override State CurrentStateObject => currentState;
+        protected TS currentState;
+        protected List<TS> states = new();
+
+        private void CreateState(Type stateType)
         {
-            public abstract State currentStateObject { get; }
+            TS newState = (TS)Activator.CreateInstance(stateType);
+            newState.Initialize((TSM)this);
+            states.Add(newState);
         }
+        /// <returns>
+        /// The state of this SM of the given type.
+        /// </returns>
+        protected TS GetStateOf(Type stateType)
+        {
+            TS state = states.FirstOrDefault(state => stateType.IsInstanceOfType(state));
+            if (state == null) CreateState(stateType);
+            return state;
+        }
+        /// <returns>
+        /// The state of this SM of type T.
+        /// A new state of type T is added to the SM if it doesn't have one already.
+        /// </returns>
+        protected TS GetStateOfType<TS2>() where TS2 : TS, new() => GetStateOf(typeof(TS2));
 
         /// <summary>
-        /// Base state machine class. Inherit from it to create your own.
+        /// Updates the current state and handle transitions that might happen.
         /// </summary>
-        public abstract class StateMachine<TStateMachine, TState, TStateInput> : StateMachine
-            where TState : State<TStateMachine, TState, TStateInput>, new()
-            where TStateMachine : StateMachine<TStateMachine, TState, TStateInput>
+        public virtual void UpdateStateMachine(TSI context)
         {
-            public override State currentStateObject => currentState;
-            public TState currentState;
-            protected Dictionary<Type, TState> states = new Dictionary<Type, TState>();
-            /// <summary>
-            /// Adds a state to the SM, the state will not be added if the SM already has a state of that type.
-            /// </summary>
-            protected void AddState(TState stateType)
+            foreach(State state in states)
             {
-                if (states.ContainsKey(stateType.GetType()))
+                if (state == null) continue;
+                state.GeneralUpdate();
+                if (state != currentState) state.InactiveUpdate();
+            }
+            if (currentState == null) return;
+            Type stateType = currentState.ActiveUpdate(context).type;
+            SetState(stateType);
+        }
+
+        /// <summary>
+        /// Transitions to the SM's state of the same type of the given state type.
+        /// </summary>
+        public void SetState<DTS>() where DTS : TS => SetState(typeof(DTS));
+        public void AddState<DTS>(DTS state) where DTS : TS
+        {
+            TS existingState = states.FirstOrDefault(state => typeof(DTS).IsInstanceOfType(state));
+            if (existingState != null) return;
+            state.Initialize((TSM)this);
+            states.Add(state);
+        }
+        public void RemoveState<DTS>() where DTS : TS
+        {
+            for (int i = 0; i < states.Count; i++)
+            {
+                if (typeof(DTS).IsInstanceOfType(states[i]))
                 {
-                    Debug.LogWarning("Adding a duplicate state of type " + stateType.GetType() + " on state machine in " + name);
+                    states.RemoveAt(i);
                     return;
                 }
-                TState newState = new TState();
-                newState.Initialize((TStateMachine)this);
-                states.Add(newState.GetType(), newState);
             }
-            /// <summary> Adds a state of type T to the SM, it will not be added if the SM already has a state of type T. </summary>
-            protected void AddState<T>() where T : TState, new() => AddState(new TState());
-            protected void SetState<T>() where T : TState, new() => HandleTransition(new T());
-            /// <returns>
-            /// The state of this SM of the same type as the given state.
-            /// The state is added to the SM if the SM doesn't have one of its type.
-            /// </returns>
-            protected TState GetStateOf(TState stateType)
+        }
+        /// <summary>
+        /// Transitions to the SM's state of the same type of the given state, caling OnExit and OnEnter.
+        /// If the given state is null, or is of the same type as the current state, the current state remains the same.
+        /// </summary>
+        /// <param name="state">
+        /// The state of the type you want to transition to
+        /// </param>
+        private void SetState(Type stateType)
+        {
+            if (stateType != null) return;
+            if (currentState != null)
             {
-                if (!states.ContainsKey(stateType.GetType())) AddState(stateType);
-                return states[stateType.GetType()];
+                if (stateType == currentState.GetType()) return;
+                currentState.OnExit((TSM)this);
             }
-            protected void UpdateStateMachine(TStateInput input)
-            {
-                if (currentState == null) return;
-                TState newState = currentState.Update(input);
-                HandleTransition(newState);
-            }
-            protected void HandleTransition(TState newState)
-            {
-                if (newState == null) return;
-                if (newState.GetType() == currentState?.GetType()) return;
-                currentState?.OnExit();
-                currentState = GetStateOf(newState);
-                currentState.OnEnter();
-            }
+            currentState = GetStateOf(stateType);
+            currentState.OnEnter((TSM)this);
+            LogStateChange(currentState);
         }
     }
 }
