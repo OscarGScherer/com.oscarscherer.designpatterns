bef249d0b9134e40806d7ab57e43ef0ccd561ed9
OscarGScherer <oscarscherer2020@gmail.com>
2025-10-13 23:48:51 -0300

    Vector extensions and coroutines class

---
+++ Runtime/Extensions/Coroutine.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace DesignPatterns
+{
+    public static class Coroutines
+    {
+        public enum TimeMode { Scaled, Unscaled, FixedScaled, FixedUnscaled }
+        private static float ScaledDeltaTime() => Time.deltaTime;
+        private static float UnscaledDeltaTime() => Time.unscaledDeltaTime;
+        private static float ScaledFixedDeltaTime() => Time.fixedDeltaTime;
+        private static float UnscaledFixedDeltaTime() => Time.fixedUnscaledDeltaTime;
+        private static Func<float>[] DeltaTimes = { ScaledDeltaTime, UnscaledDeltaTime, ScaledFixedDeltaTime, UnscaledFixedDeltaTime };
+
+        public static IEnumerator Lerp(
+            float duration, Func<YieldInstruction> step = null, 
+            Action enter = null, Action<float> update = null, Action exit = null,
+            TimeMode timeMode = TimeMode.Scaled
+        )
+        {
+            enter?.Invoke();
+            float timer = 0;
+            duration = duration < 0 ? 0 : duration;
+            update ??= _ => { };
+            step ??= () => null;
+            Func<float> deltaTime = DeltaTimes[(int)TimeMode.Scaled];
+            while (timer < duration)
+            {
+                update(timer / duration);
+                timer += deltaTime();
+                yield return step.Invoke();
+            }
+            exit?.Invoke();
+        }
+    }
+}
--- Runtime/Extensions/VectorExtensions.cs
+++ Runtime/Extensions/VectorExtensions.cs
@@ -4,11 +4,25 @@ namespace DesignPatterns
 {
     public static class VectorExtensions
     {
-        public static float Angle360(this Vector3 to, Vector3 from, Vector3 axis)
+        public static float Angle360(Vector3 from, Vector3 to, Vector3 axis)
         {
             float signedAngle = Vector3.SignedAngle(from, to, axis);
             signedAngle = signedAngle < 0 ? signedAngle + 360f : signedAngle;
             return signedAngle;
         }
+
+        // Swizzling
+        // V2
+        public static Vector2 Swizzle(this Vector4 self, int x, int y) => new Vector4(self[x], self[y]);
+        public static Vector2 Swizzle(this Vector3 self, int x, int y) => new Vector2(self[x], self[y]);
+        public static Vector2 Swizzle(this Vector2 self, int x, int y) => new Vector2(self[x], self[y]);
+        // V3
+        public static Vector3 Swizzle(this Vector4 self, int x, int y, int z) => new Vector3(self[x], self[y], self[z]);
+        public static Vector3 Swizzle(this Vector3 self, int x, int y, int z) => new Vector3(self[x], self[y], self[z]);
+        public static Vector3 Swizzle(this Vector2 self, int x, int y, int z) => new Vector3(self[x], self[y], self[z]);
+        // V4
+        public static Vector4 Swizzle(this Vector4 self, int x, int y, int z, int w) => new Vector4(self[x], self[y], self[z], self[w]);
+        public static Vector4 Swizzle(this Vector3 self, int x, int y, int z, int w) => new Vector4(self[x], self[y], self[z], self[w]);
+        public static Vector4 Swizzle(this Vector2 self, int x, int y, int z, int w) => new Vector4(self[x], self[y], self[z], self[w]);
     }
 }
